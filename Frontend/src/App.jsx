ac4a28e5c333ced0bf5ed4c0ab256b72e9ee1ce6
Melih DİNÇER <m_dincer41@hotmail.com>
2025-05-15 20:03:19 +0300

    15 Mayıs second commit. -Cart modeli oluşturuldu ve dialog kutusu eklendi.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import PizzaList from "./components/PizzaList";
 import ThemeSelector from "./components/ThemeSelector";
 import { ThemeContext } from "./contexts/ThemeContext";
+import Cart from "./components/Cart";
 
 export default function App() {
   const { mode } = useContext(ThemeContext);
@@ -18,6 +19,7 @@ export default function App() {
         <ThemeSelector />
         <div className="container my-4">
           <PizzaList />
+          <Cart />
         </div>
       </div>
     </>
